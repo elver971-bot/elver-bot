c4dc91628ca252aaf6e67b6e6693d30770ca3054
Elver <elver971@yandex.ru>
2026-05-08 22:20:15 +0300

    правки

--- bot.py
+++ bot.py
@@ -165,15 +165,15 @@ SYSTEM_PROMPT = """
 def start(message):
     chat_id = message.chat.id
 
-    # очистка прошлого диалога
+    # полная очистка прошлого диалога
+    if chat_id in user_memory:
+    del user_memory[chat_id]
+
     if chat_id in lead_state:
-        del lead_state[chat_id]
+    del lead_state[chat_id]
 
     if chat_id in lead_data:
-        del lead_data[chat_id]
-
-    if chat_id in user_memory:
-        del user_memory[chat_id]
+    del lead_data[chat_id]
 
     lead_state[chat_id] = "wait_niche"
     lead_data[chat_id] = {}
