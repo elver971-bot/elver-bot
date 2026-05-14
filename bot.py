4078b870b3b262f0c04de657b6ebaacd5e2e11f3
Elver <elver971@yandex.ru>
2026-05-14 11:44:32 +0300

    патерн

--- bot.py
+++ bot.py
@@ -4,6 +4,13 @@ import json
 
 phone_pattern = r"(\+7|8)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}"
 
+phone_match = re.search(phone_pattern, message.text)
+
+extracted_phone = None
+
+if phone_match:
+    extracted_phone = phone_match.group(0)
+
 from datetime import datetime, timedelta
 
 from flask import Flask, request
